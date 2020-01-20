95e34c6f10041c390ee5428961b504c2e9eb6c9b
Bartosz Gromada <bartosz.k.gromada@gmail.com>
2020-01-20 21:54:00 +0100

    bug

---
+++ src/redux/tripsRedux.js
@@ -0,0 +1,81 @@
+/* SELECTORS */
+
+export const getAllTrips = ({trips}) => trips;
+
+export const getFilteredTrips = ({trips, filters}) => {
+  let output = trips;
+
+  // filter by search phrase
+  if(filters.searchPhrase){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+  // TODO - filter by duration
+  if(filters.duration){
+    const pattern = new RegExp(filters.duration, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+  // TODO - filter by tags
+  if(filters.tags){
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.tags));
+  }
+
+  // TODO - sort by cost descending (most expensive goes first)
+  if(filters.cost){
+    const pattern = new RegExp(filters.cost, 'i');
+    output = output.filter(trip => pattern.test(trip.cost));
+  }
+
+  return output;
+};
+
+export const getTripById = ({trips}, tripId) => {
+  const filtered = trips;
+
+  // TODO - filter trips by tripId
+
+  console.log('filtering trips by tripId:', tripId, filtered);
+  return filtered.length ? filtered[0] : {error: true};
+};
+
+export const getTripsForCountry = ({trips}, countryCode) => {
+  const filtered = trips;
+
+  // TODO - filter trips by countryCode
+
+  console.log('filtering trips by countryCode:', countryCode, filtered);
+  return filtered.length ? filtered : [{error: true}];
+};
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'trips';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const GET_TRIPS = createActionName('GET_TRIPS');
+
+// action creators
+export const createActionGetTrips = payload => ({ payload: { ...payload }, type: GET_TRIPS });
+
+// reducer
+export default function reducer(state = [], action = {}) {
+  switch (action.type) {
+    case GET_TRIPS:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
+//
+// reducer
+//export default function reducer(statePart = [], action = {}) {
+//  switch (action.type) {
+//    default:
+//      return statePart;
+//  }
+//}
